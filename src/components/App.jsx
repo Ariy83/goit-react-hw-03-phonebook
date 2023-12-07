82ff5dae158f83b78228abb4fbafb5a9446c787c
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-12-07 17:18:57 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,24 @@ import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
 import { nanoid } from "nanoid";
 import { ContactForm } from "./ContactForm/ContactForm";
+import storage from "./storage/storage";
 
 export class App extends React.Component {
   state = {
     contacts: [],
     filter: '',
+  }
+
+  componentDidMount() {
+    const contacts = storage.load(storage.KEY);
+    if (contacts?.length) {
+      this.setState({contacts})
+    }
+  }
+  componentDidUpdate(_,prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      storage.save(storage.KEY, this.state.contacts)
+    }
   }
   
   handleAddContact = ({ name, number }) => {
